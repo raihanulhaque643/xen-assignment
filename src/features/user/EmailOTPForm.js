d1fa4dd8fe2de91779eefc49ec912ee33dcb2136
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-01 23:46:11 +0600

    send email/sms otp will show success msg after 3second delay

--- src/features/user/EmailOTPForm.js
+++ src/features/user/EmailOTPForm.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const EmailOTPForm = () => {
+
+    const [sendingOTP, setSendingOTP] = useState(false)
+    const [sendingOTPcompleted, setSendingOTPcompleted] = useState(false)
+
     return (
+        <div className="">
+            {
+                sendingOTPcompleted &&
+                <div className="w-full text-center font-semibold text-lg mb-2">OTP has been sent to your <span className="uppercase font-bold">Email</span></div>
+            }
             <Formik
                 initialValues={{ email: '' }}
                 validate={values => {
@@ -13,10 +22,14 @@ const EmailOTPForm = () => {
                     return errors;
                 }}
                 onSubmit={(values, { setSubmitting }) => {
+                    setSendingOTPcompleted(false)
+                    setSendingOTP(true)
                     setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
+                    // alert(JSON.stringify(values, null, 2));
+                    setSendingOTP(false)
+                    setSendingOTPcompleted(true)
                     setSubmitting(false);
-                    }, 400);
+                    }, 3000);
                 }}
                 >
                 {({ isSubmitting }) => (
@@ -25,13 +38,20 @@ const EmailOTPForm = () => {
                     <Field placeholder="Email OTP" className="w-full rounded p-2 my-2 border-b-2 border-blue-500 text-center" type="text" name="email"/>
                     <ErrorMessage name="email" component="small" className="text-red-600"  />
 
-                    <button className="bg-black rounded text-blue-200 font-semibold text-lg p-2 my-2 border-2 border-yellow-600" type="submit" disabled={isSubmitting}>
-                        Submit
+                    <button
+                    className="bg-black rounded text-blue-200 font-semibold text-lg p-2 my-2 border-2 border-yellow-600 flex flex-row justify-center items-center" type="submit" disabled={isSubmitting}>
+                        <span>Submit</span>
+                        {
+                            sendingOTP &&
+                            <svg className="rounded-full animate-ping duration-300 w-3 h-3 border-2 mx-2"></svg>
+                        }
                     </button>
 
                     </Form>
                 )}
                 </Formik>
+        </div>
+
     )
 }
 
--- src/features/user/SmsOTPForm.js
+++ src/features/user/SmsOTPForm.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const SmsOTPForm = () => {
+
+    const [sendingOTP, setSendingOTP] = useState(false)
+    const [sendingOTPcompleted, setSendingOTPcompleted] = useState(false)
+
     return (
+        <div className="">
+            {
+                sendingOTPcompleted &&
+                <div className="w-full text-center font-semibold text-lg mb-2">OTP has been sent to your <span className="uppercase font-bold">Phone</span></div>
+            }
         <Formik
                 initialValues={{ phone: '' }}
                 validate={values => {
@@ -13,10 +22,14 @@ const SmsOTPForm = () => {
                     return errors;
                 }}
                 onSubmit={(values, { setSubmitting }) => {
+                    setSendingOTP(true)
+                    setSendingOTPcompleted(false)
                     setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
+                    // alert(JSON.stringify(values, null, 2));
+                    setSendingOTP(false)
+                    setSendingOTPcompleted(true)
                     setSubmitting(false);
-                    }, 400);
+                    }, 3000);
                 }}
                 >
                 {({ isSubmitting }) => (
@@ -25,13 +38,18 @@ const SmsOTPForm = () => {
                     <Field placeholder="SMS OTP" className="w-full rounded p-2 my-2 border-b-2 border-blue-500 text-center" type="text" name="phone"/>
                     <ErrorMessage name="phone" component="small" className="text-red-600"  />
 
-                    <button className="bg-black rounded text-blue-200 font-semibold text-lg p-2 my-2 border-2 border-yellow-600" type="submit" disabled={isSubmitting}>
-                        Submit
+                    <button className="bg-black rounded text-blue-200 font-semibold text-lg p-2 my-2 border-2 border-yellow-600  flex flex-row justify-center items-center" type="submit" disabled={isSubmitting}>
+                        <span>Submit</span>
+                        {
+                            sendingOTP &&
+                            <svg className="rounded-full animate-ping duration-300 w-3 h-3 border-2 mx-2"></svg>
+                        }
                     </button>
 
                     </Form>
                 )}
                 </Formik>
+        </div>
     )
 }
 
