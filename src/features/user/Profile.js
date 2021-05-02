31e5fd4e09a123b4eb7eb4595972f8d7a06a987e
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-02 14:36:39 +0600

    profile page logout button placed

--- src/features/user/Profile.js
+++ src/features/user/Profile.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Profile = () => {
     return (
-        <div>
-            profile comp...
+        <div className="border w-full max-w-md p-4 flex flex-row items-center justify-between bg-gray-200">
+            <div className="flex flex-col font-semibold">
+                Profile Component
+            </div>
+            <div className="flex flex-col">
+                <button className="border border-red-700 border-b-2 p-2 rounded font-bold">Logout</button>
+            </div>
         </div>
     )
 }
